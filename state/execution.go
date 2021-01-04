dada4c7f0285c72323b97ee5870176210175be77
yqq <youngqqcn@163.com>
2021-01-04 17:07:03 +0800

    fix replay bug

--- state/execution.go
+++ state/execution.go
@@ -315,6 +315,17 @@ func execBlockOnProxyApp(
 
 func getBeginBlockValidatorInfo(block *types.Block, lastValSet *types.ValidatorSet, stateDB dbm.DB) (abci.LastCommitInfo, []abci.Evidence) {
 
+	// 2021-01-04 fix issue(bug): https://github.com/orientwalt/htdf/issues/19
+	// For replaying blocks, load history validator set
+	state := LoadState(stateDB)
+	if block.Height > 1 && block.Height != state.LastBlockHeight+1 {
+		var err error
+		lastValSet, err = LoadValidators(stateDB, block.Height-1)
+		if err != nil {
+			panic(fmt.Sprintf("failed to load validatorset at heith %d", state.LastBlockHeight))
+		}
+	}
+
 	// Sanity check that commit length matches validator set size -
 	// only applies after first block
 	if block.Height > 1 {
